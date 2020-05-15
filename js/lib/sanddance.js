c86ddceccb840142e03cd5c227f32930afa424de
satotake <doublequotation@gmail.com>
2020-05-15 19:22:50 +0900

    initial commit

--- js/lib/sanddance.js
+++ js/lib/sanddance.js
@@ -1,4 +1,4 @@
-import * as widgets from '@jupyter-widgets/base';
+import { DOMWidgetModel, DOMWidgetView } from '@jupyter-widgets/base';
 import * as deck from '@deck.gl/core';
 import * as layers from '@deck.gl/layers';
 import * as luma from 'luma.gl';
@@ -7,49 +7,103 @@ import * as vega from 'vega';
 import { Explorer, use } from '@msrvida/sanddance-explorer';
 import ReactDOM from 'react-dom'
 import React from 'react'
-
-import './style.css';
+import './sanddance-explorer.css';
 
 use(fabric, vega, deck, layers, luma);
-
 fabric.initializeIcons();
 
-// See sanddance.py for the kernel counterpart to this file.
+export class SandDanceModel extends DOMWidgetModel {
+    get defaults () {
+        return {
+            ...super.defaults(),
+            _model_name : 'SandDanceModel',
+            _view_name : 'SandDanceView',
+            _model_module : 'pandas-sanddance',
+            _view_module : 'pandas-sanddance',
+            _model_module_version : '0.1.0',
+            _view_module_version : '0.1.0',
+            value : '[]',  // json string
+            width : '100%',  // json string
+            heigth : '60vh',  // json string
+            snapshots: [],
+        }
+    }
+}
 
-export var SandDanceModel = widgets.DOMWidgetModel.extend({
-    defaults: Object.assign(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'SandDanceModel',
-        _view_name : 'SandDanceView',
-        _model_module : 'pandas-sanddance',
-        _view_module : 'pandas-sanddance',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
-        value : {data: '', dtypes: {}},
-    })
-});
+export class SandDanceView extends DOMWidgetView {
+    constructor(options) {
+        super(options)
+        this._explorer = null;
+        this._wrapper = null;
+        this.intervalID = null;
+    }
 
 
-// Custom View. Renders the widget model.
-export var SandDanceView = widgets.DOMWidgetView.extend({
-    // Defines how the widget gets rendered into the DOM
-    render: function() {
+    render () {
 
         const explorerProps = {
             logoClickUrl: 'https://microsoft.github.io/SandDance/',
+            compactUI: true,
             mounted: explorer => {
-                explorer.load([]);
-                this.explorer = explorer;
+                this._explorer = explorer;
                 this.model.on('change:value', this.value_changed, this);
+
+                setTimeout(() => {
+                    this.value_changed();
+                }, 1);
             },
+            // TODO
+            ref: ref => {
+                // restore previoous snapshot states
+                ref.state.snapshots = this.model.get('snapshots');
+                this.intervalID = this.autosaveSnapshots(ref)
+            },
+            key: 'explorer-key'
         };
 
-        ReactDOM.render(React.createElement(Explorer, explorerProps));
-    },
 
-    value_changed: function() {
-        console.log(JSON.parse(this.model.get('value')));
-        this.explorer.load(JSON.parse(this.model.get('value')));
+        this._wrapper = React.createElement(
+            'div',
+            {
+                style: {
+                    width: this.model.get('width'),
+                    height: this.model.get('height'),
+                }
+            },
+            [React.createElement(Explorer, explorerProps)],
+        );
+
+        ReactDOM.render(this._wrapper, this.el);
+
+        this.model.on('change:width', this.size_changed, this);
+        this.model.on('change:height', this.size_changed, this);
     }
-});
 
+    // TODO
+    autosaveSnapshots (ref) {
+        return setInterval(() => {
+            this.model.set('snapshots', ref.state.snapshots)
+            this.model.save_changes();
+        }, 1000 * 30);
+    }
 
+    size_changed () {
+        const style = {
+            width: this.model.get('width'),
+            height: this.model.get('height'),
+        };
+        this._wrapper.props.style = style;
+    }
+
+    value_changed () {
+        this._explorer.load(JSON.parse(this.model.get('value')));
+    }
+
+    remove() {
+        if (this.intervalID) {
+            clearInterval(this.intervalID);
+        }
+        super.remove();
+    }
+
+}
--- pandas_sanddance/sanddance.py
+++ pandas_sanddance/sanddance.py
@@ -1,11 +1,18 @@
 import ipywidgets as widgets
 from traitlets import (Unicode, List, Instance, observe)
 import pandas as pd
+from IPython.core.display import display
 
 # See js/lib/sanddance.js for the frontend counterpart to this file.
+#
+defaults = {
+    'width': '100%',
+    'height': '60vh',
+    'value': '[]',
+}
 
 @widgets.register
-class SandDance(widgets.DOMWidget):
+class SandDanceWidget(widgets.DOMWidget):
     """An SandDance widget."""
 
     # Name of the widget view class in front-end
@@ -29,12 +36,22 @@ class SandDance(widgets.DOMWidget):
     # Widget properties are defined as traitlets. Any property tagged with `sync=True`
     # is automatically synced to the frontend *any* time it changes in Python.
     # It is synced back to Python from the frontend *any* time the model is touched.
-    value = Unicode('{}').tag(sync=True)
+    value = Unicode(defaults['value']).tag(sync=True)
     df = Instance(klass=pd.DataFrame)
+
+    width = Unicode(defaults['width']).tag(sync=True)
+    height = Unicode(defaults['height']).tag(sync=True)
+    snapshots = List([]).tag(sync=True)
 
     @observe('df')
     def _observe_df(self, change):
         new = change['new']
-        # dtypes = {k: str(v) for k, v in new.dtypes.to_dict().items()}
-        # self.value = {'data': new.to_json(), 'dtypes': dtypes}
         self.value = new.to_json(orient='records')
+
+    def show(self, **kwargs):
+        self.width = kwargs.get('width', defaults['width']);
+        self.height = kwargs.get('height', defaults['height']);
+        display(self)
+
+    def load(self, df):
+        self.df = df
